d3392ab874e14edef3486277f78994b7a6cb5dfa
ibrahimhblack <ibrahim.maghfoul@gmail.com>
2021-11-23 22:17:59 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breathAnimation() {
+  text.innerHTML = "Breath In!";
+  container.className = "container grow";
+  setTimeout(() => {
+    text.innerHTML = "Hold";
+    setTimeout(() => {
+      text.innerHTML = "Breath Out!";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
